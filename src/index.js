7b10f72c66818d5e5ae1cff18c8008d1da5611b3
kvr2834 <kartikvaria101@gmail.com>
2025-07-26 17:02:05 +0530

    limit increased

--- src/index.js
+++ src/index.js
@@ -27,7 +27,9 @@ app.use(
 );
 
 
-app.use(express.json());
+app.use(express.json({ limit: '15mb' }));
+app.use(express.urlencoded({ extended: true, limit: '15mb' }));
+
 
 
 
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -9,7 +9,7 @@ router.post('/signup', signup)
 router.post('/signin', signin)
 router.post('/logout', logout)
 
-router.put('/update-profile', express.json({ limit: '10mb' }),protectRoute, updateProfile)
+router.put('/update-profile',protectRoute, updateProfile)
 
 router.get('/check', protectRoute, checkAuth)
 
