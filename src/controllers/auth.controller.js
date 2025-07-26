ec229d1fb79a0c1eb3beed59a47a38dead30e508
kvr2834 <kartikvaria101@gmail.com>
2025-07-26 15:31:27 +0530

    Profile picture error solved

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -94,7 +94,7 @@ export const updateProfile = async (req, res) => {
     const uploadResponse = await cloudinary.uploader.upload(profilePic);
     const updatedUser = await User.findByIdAndUpdate(
       userId,
-      { profilePic: uploadResponse.secure_url },
+      { profilepic: uploadResponse.secure_url },
       { new: true }
 
     )
